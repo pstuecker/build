f29aeb0a2205aebb310dcdef67bdaba6c3cf4565
Pascal Stücker <stuecker.pascal@scheidt-bachmann-st.de>
2022-11-09 11:29:24 +0100

    Fix string escapes

--- vars/collectDependencies.groovy
+++ vars/collectDependencies.groovy
@@ -8,7 +8,7 @@ def call() {
     dir("$WORKSPACE/__dependencies")
     {
         sh 'find .. -name \'package-lock.json\' | awk \'{ f = substr($0, 4); gsub("/", "_", f); print $0 " " f}\' | xargs -L1 cp'
-        sh "find .. -name '*.deps' -exec cp {} . \;"
+        sh 'find .. -name \'*.deps\' -exec cp {} . \\;'
         sh 'jar -cfM dependencies.zip ./*'
         archiveArtifacts artifacts: 'dependencies.zip'
     }
