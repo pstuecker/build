3198c92f7a55ddf28ddc97b3afbfe64edd25ecc8
Pascal Stücker <stuecker.pascal@scheidt-bachmann-st.de>
2022-11-15 09:53:31 +0100

    Rewrite p2-maven dependencies to their maven GAVs

--- utils/process.py
+++ utils/process.py
@@ -10,24 +10,34 @@ import zipfile
 import sys
 import subprocess
 import os
+import re
 
 if len(sys.argv) < 3:
     print("Usage: process.py in.zip outfile")
     sys.exit(-1)
+
+def processDependency(dep):
+    result = re.search(r"p2.eclipse.plugin:maven\.([a-z-.]+)\.artifact\.([a-z-.]+):eclipse-plugin:([0-9\.]+):system", dep)
+    if not result:
+        return dep 
+    else:
+        return f"{result.group(1)}:{result.group(2)}:{result.group(3)}:compile"
 
 with zipfile.ZipFile(sys.argv[1], 'r') as zf:
     for filename in zf.namelist():
         print("Processing", filename)
 
         data = zf.read(filename)
+        data = "\n".join(map(processDependency, data.decode("utf-8").splitlines()))
+
         # npm lock files must be called exactly package-lock.json for Dash to process
         fname = 'package-lock.json' if filename.endswith('.json') else filename
-        with open(fname, 'wb') as f:
+        with open(fname, 'w') as f:
             f.write(data)
         args = 'java -Djava.net.useSystemProxies=true -jar dash.jar -summary DEPENDENCIES ' + fname
         subprocess.run(args, shell=True, check=False)
         os.remove(fname)
-        
+
         # append to outfile 
         with open('DEPENDENCIES', 'r') as infile:
             with open(sys.argv[2], 'a+') as outfile:
