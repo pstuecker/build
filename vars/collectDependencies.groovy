cdb98a6a4029bcbd69803807baa0f8b555e47f05
Pascal Stücker <stuecker.pascal@scheidt-bachmann-st.de>
2022-11-17 10:43:33 +0100

    Ignore empty dependencies in collectDependencies

--- vars/collectDependencies.groovy
+++ vars/collectDependencies.groovy
@@ -7,7 +7,7 @@
 def call() {
     dir("$WORKSPACE/__dependencies")
     {
-        sh 'find .. -name \'package-lock.json\' | awk \'{ f = substr($0, 4); gsub("/", "_", f); print $0 " " f}\' | xargs -L1 cp'
+        sh 'find .. -name \'package-lock.json\' | awk \'{ f = substr($0, 4); gsub("/", "_", f); print $0 " " f}\' | xargs -r -L1 cp'
         sh 'find .. -name \'*.deps\' -exec cp {} . \\;'
         sh 'jar -cfM dependencies.zip ./*'
         archiveArtifacts artifacts: 'dependencies.zip'
