f8e3b898ab2af83b67cfc9d3167d54d766e5cd70
Ngoc Quang Truong <truong.quang@scheidt-bachmann-st.de>
2022-11-22 09:58:35 +0000

    Update removeOldSnapshot.groovy

--- vars/removeOldSnapshot.groovy
+++ vars/removeOldSnapshot.groovy
@@ -6,15 +6,21 @@
 def call(Map args) {
     container('jnlp') {
         sshagent (['projects-storage.eclipse.org-bot-ssh']) {
-sh '''#!/bin/bash
-            snapshots=( \$(ssh -o BatchMode=yes genie.set@projects-storage.eclipse.org ls /home/data/httpd/download.eclipse.org/set/snapshots/bin/${args.repo}/feature) )
-            readarray -t branches < <(git branch)
-            for i in \$[!branches[@]}; do copy=\$(echo \${branches[\$i]} | sed 's/[ *]\\+//g'; branches[\$i]=\$copy; done
-            for snapshot in \$snapshots; do snapshotName=\$(echo \$snapshot | sed 's/\\///g; if [[ ! \${branches[@]} =~ feature/\${snapshotName} ]];
-            then
-                ssh -o BatchMode=yes genie.set@projects-storage.eclipse.org rm -rf /home/data/httpd/download.eclipse.org/set/snapshots/bin/${args.repo}/\${snapshot};
-            fi
+            sh '''#!/bin/bash
+                readarray -t snapshot < <(ssh -o BatchMode=yes genie.set@projects-storage.eclipse.org ls /home/data/httpd/download.eclipse.org/set/snapshots/bin/${args.repo}/feature)
+                readarray -t branches < <(git branch)
+                for i in \$[!branches[@]}
+                do 
+                    copy=\$(echo \${branches[\$i]} | sed 's/[ *]\\+//g')
+                    branches[\$i]=\$copy
+                done
+                for snapshot in \$snapshots
+                do 
+                    snapshotName=\$(echo \$snapshot | sed 's/\\///g
+                    if [[ ! \${branches[@]} =~ feature/\${snapshotName} ]]
+                    then
+                        ssh -o BatchMode=yes genie.set@projects-storage.eclipse.org rm -rf /home/data/httpd/download.eclipse.org/set/snapshots/bin/${args.repo}/\${snapshot}
+                    fi
+                done
             '''
-        }
-    }
 }
