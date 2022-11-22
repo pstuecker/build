d391fcf53216be1bc7fc902d0d09b53263d8e457
Ngoc Quang Truong <truong.quang@scheidt-bachmann-st.de>
2022-11-22 12:09:17 +0000

    debug code

--- vars/removeOldSnapshot.groovy
+++ vars/removeOldSnapshot.groovy
@@ -10,11 +10,14 @@ def call(Map args) {
                 """#!/bin/bash
                     readarray -t snapshots < <(ssh -o BatchMode=yes genie.set@projects-storage.eclipse.org ls /home/data/httpd/download.eclipse.org/set/snapshots/${args.repo}/feature)
                     readarray -t branches < <(git branch| sed 's/[ *]\\+//g')
+                    declare -p branches
                     for snapshot in \$snapshots
                     do 
                         snapshotName=\$(echo \$snapshot | sed 's/\\///g')
+                        echo $snapshot
                         if [[ ! \${branches[@]} =~ feature/\${snapshotName} ]]
                         then
+                            echo Delete
                             ssh -o BatchMode=yes genie.set@projects-storage.eclipse.org rm -rf /home/data/httpd/download.eclipse.org/set/snapshots/${args.repo}/\${snapshot}
                         fi
                     done
