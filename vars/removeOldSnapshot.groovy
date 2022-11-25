d4c9cc398ecbc57bd36f850713648a9ce9ba1489
Ngoc Quang Truong <truong.quang@scheidt-bachmann-st.de>
2022-11-25 11:56:40 +0000

    removeSnapshot: change get branch command

--- vars/removeOldSnapshot.groovy
+++ vars/removeOldSnapshot.groovy
@@ -9,7 +9,7 @@ def call(Map args) {
             sh (
                 """#!/bin/bash
                     readarray -t snapshots < <(ssh -o BatchMode=yes genie.set@projects-storage.eclipse.org ls /home/data/httpd/download.eclipse.org/set/snapshots/${args.repo}/feature)
-                    readarray -t branches < <(git branch -a | grep -o 'feature/.*')
+                    readarray -t branches < <(git ls-remote --heads https://gitlab.eclipse.org/eclipse/set/set.git/ | grep -o 'feature/.*')
                     for snapshot in \${snapshots[@]}
                     do
                         if [[ ! \${branches[@]} =~ feature/\${snapshot} ]]
