96efbf9ba5c0cc644a3ff8e5ad0fe3cd708751e1
Ngoc Quang Truong <truong.quang@scheidt-bachmann-st.de>
2022-11-25 08:33:55 +0000

    fix syntax error

--- vars/removeOldSnapshot.groovy
+++ vars/removeOldSnapshot.groovy
@@ -9,7 +9,7 @@ def call(Map args) {
             sh (
                 """#!/bin/bash
                     readarray -t snapshots < <(ssh -o BatchMode=yes genie.set@projects-storage.eclipse.org ls /home/data/httpd/download.eclipse.org/set/snapshots/${args.repo}/feature)
-                    readarray -t branches < <(git branch -a | sed 's/[A-Za-z * \/]\+\/feature\///g')
+                    readarray -t branches < <(git branch -a | sed 's/[A-Za-z * \\/]\\+\\/feature\\///g')
                     for snapshot in \${snapshots[@]}
                     do
                         if [[ ! \${branches[@]} =~ ${snapshot} ]]
