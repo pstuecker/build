3ea60017aeabfd4e6e0e15dc01a25d57bf4d8c7b
Pascal Stücker <stuecker.pascal@scheidt-bachmann-st.de>
2023-05-12 07:09:33 +0200

    Fall back to BUILD_ENV if branch for deployment is not given

--- vars/deployProduct.groovy
+++ vars/deployProduct.groovy
@@ -7,9 +7,9 @@
 def call(Map args) {
     container('jnlp') {
         sshagent (['projects-storage.eclipse.org-bot-ssh']) {
-            sh "ssh -o BatchMode=yes genie.set@projects-storage.eclipse.org rm -rf /home/data/httpd/download.eclipse.org/set/nightly/${args.branch}/bin/${args.name}"
-            sh "ssh -o BatchMode=yes genie.set@projects-storage.eclipse.org mkdir -p /home/data/httpd/download.eclipse.org/set/nightly/${args.branch}/bin/${args.name}"
-            sh "scp -o BatchMode=yes -r ${args.path} genie.set@projects-storage.eclipse.org:/home/data/httpd/download.eclipse.org/set/nightly/${args.branch}/bin/${args.name}"
+            sh "ssh -o BatchMode=yes genie.set@projects-storage.eclipse.org rm -rf /home/data/httpd/download.eclipse.org/set/nightly/${args.branch ?: BRANCH_NAME}/bin/${args.name}"
+            sh "ssh -o BatchMode=yes genie.set@projects-storage.eclipse.org mkdir -p /home/data/httpd/download.eclipse.org/set/nightly/${args.branch ?: BRANCH_NAME}/bin/${args.name}"
+            sh "scp -o BatchMode=yes -r ${args.path} genie.set@projects-storage.eclipse.org:/home/data/httpd/download.eclipse.org/set/nightly/${args.branch ?: BRANCH_NAME}/bin/${args.name}"
         }
     }
 }
